4e6d35e752773f295205dd7947d041dd295e1be7
Rangga <ranggajatun12@gmail.com>
2025-08-26 07:54:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyACUEZHJYVkqH1WbXSX4ra4nhpdQMbNv0U",
+    authDomain: "stok-gudanggg.firebaseapp.com",
+    projectId: "stok-gudanggg",
+    storageBucket: "stok-gudanggg.firebasestorage.app",
+    messagingSenderId: "274647379987",
+    appId: "1:274647379987:web:6bd87b8925284830958fe6",
+    measurementId: "G-D6ERVDG8S5"
+  };
